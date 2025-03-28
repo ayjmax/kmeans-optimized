67c56f08f05396a143c62c77903df0bae0007653
Alec <ayj.max@gmail.com>
2025-03-27 23:48:34 -0400

    Updated run.sh

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ cat ${DATASET} | bin/kmeans-serial >> output.txt
 echo "------------------------- Serial Fast -------------------------" >> output.txt
 cat ${DATASET} | bin/kmeans-serial-fast >> output.txt
 
-# echo "------------------------- Parallel Simple -------------------------" >> output.txt
-# cat ${DATASET} | bin/kmeans-parallel-simple >> output.txt
+echo "------------------------- Parallel Simple -------------------------" >> output.txt
+cat ${DATASET} | bin/kmeans-parallel-simple >> output.txt
 
-# echo "------------------------- Parallel TBB -------------------------" >> output.txt
-# cat ${DATASET} | bin/kmeans-parallel-tbb >> output.txt
+# echo "------------------------- Parallel Fast -------------------------" >> output.txt
+# cat ${DATASET} | bin/kmeans-parallel-fast >> output.txt
