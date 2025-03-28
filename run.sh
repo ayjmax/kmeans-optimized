5a0961e9433f2f021ff573f634df87cb6a34d82a
Alec <ayj.max@gmail.com>
2025-03-27 21:06:44 -0400

    Updated run.sh to run all kmeans variations and append to output file

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 DATASET=datasets/bean.txt
-# DATASET=datasets/dataset2.txt
 
 source oneapi-tbb-2022.0.0/env/vars.sh
 
+make clean
 make all
 
-cat ${DATASET} | bin/kmeans > output.txt
+echo "------------------------- Serial -------------------------" > output.txt
+cat ${DATASET} | bin/kmeans-serial >> output.txt
+
+echo "------------------------- Serial Fast -------------------------" >> output.txt
+cat ${DATASET} | bin/kmeans-serial-fast >> output.txt
+
+# echo "------------------------- Parallel Simple -------------------------" >> output.txt
+# cat ${DATASET} | bin/kmeans-parallel-simple >> output.txt
+
+# echo "------------------------- Parallel TBB -------------------------" >> output.txt
+# cat ${DATASET} | bin/kmeans-parallel-tbb >> output.txt
