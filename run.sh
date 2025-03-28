58a05b2e3576a58b938ada8d8cb6c591dc939318
Alec <ayj.max@gmail.com>
2025-03-28 04:01:15 -0400

    Updated run.sh

--- run.sh
+++ run.sh
@@ -7,17 +7,17 @@ make all
 
 echo "" > output.txt
 
-# echo "------------------------- Serial -------------------------" > output.txt
-# cat ${DATASET} | bin/kmeans-serial >> output.txt
+echo "------------------------- Serial -------------------------" > output.txt
+cat ${DATASET} | bin/kmeans-serial >> output.txt
 
-# echo "------------------------- Serial Fast -------------------------" >> output.txt
-# cat ${DATASET} | bin/kmeans-serial-fast >> output.txt
+echo "------------------------- Serial Fast -------------------------" >> output.txt
+cat ${DATASET} | bin/kmeans-serial-fast >> output.txt
 
-# echo "------------------------- Serial Fast Unroll -------------------------" >> output.txt
-# cat ${DATASET} | bin/kmeans-serial-fast-unroll >> output.txt
+echo "------------------------- Serial Fast Unroll -------------------------" >> output.txt
+cat ${DATASET} | bin/kmeans-serial-fast-unroll >> output.txt
 
-# echo "------------------------- Serial Fast No Cluster -------------------------" >> output.txt
-# cat ${DATASET} | bin/kmeans-serial-fast-no-cluster >> output.txt
+echo "------------------------- Serial Fast No Cluster -------------------------" >> output.txt
+cat ${DATASET} | bin/kmeans-serial-fast-no-cluster >> output.txt
 
 echo "------------------------- Parallel Simple -------------------------" >> output.txt
 cat ${DATASET} | bin/kmeans-parallel-simple >> output.txt
